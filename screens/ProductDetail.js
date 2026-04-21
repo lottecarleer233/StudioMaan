563e78774b71970636c331d6326bb107edc7e014
Lotte Carleer <lottecarleer@gmail.com>
2026-04-22 00:05:02 +0200

    lotte-tussentijdseevaluatie-nieuwe-styling-toegevoegd-productdetails

--- screens/ProductDetail.js
+++ screens/ProductDetail.js
@@ -28,7 +28,10 @@ const ProductDetail = ({ route }) => {
       <Image source={image} style={styles.image} />
        <Text style={styles.title}>{title}</Text>
       <Text style={styles.subtitle}>{subtitle}</Text>
+
+      <View style={styles.divider} />
       <Text  style={styles.price}>€{price}</Text>
+      <View style={styles.divider} />
 
       <View style={styles.quantityContainer}>
         <TouchableOpacity style={styles.button} onPress={decreaseQuantity}>
@@ -42,7 +45,10 @@ const ProductDetail = ({ route }) => {
         </TouchableOpacity>
       </View>
 
-      <Text style={styles.totalPrice}>Totaal: €{price * quantity}</Text>
+     <View style={styles.totalBox}>
+  <Text style={styles.totalLabel}>TOTAAL</Text>
+  <Text style={styles.totalPrice}>€{(price * quantity).toFixed(2)}</Text>
+</View>
 
       <StatusBar style="auto" />
     </ScrollView>
@@ -78,7 +84,6 @@ const styles = StyleSheet.create({
   },
   price: {
    fontWeight: "bold",
-   marginTop: 15,
    fontSize: 20,
   },
   button: {
@@ -101,10 +106,31 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginHorizontal: 10,
   },
-  totalPrice: {
-  fontSize: 19,
+  
+  divider: {
+  width: "80%",
+  height: 1,
+  backgroundColor: "#000",
+  marginVertical: 20,
+},
+totalBox: {
+  backgroundColor: "#fff",
+  width: "85%",
+  padding: 20,
+  borderRadius: 15,
+  alignItems: "center",
+},
+
+totalLabel: {
+  fontSize: 16,
+  color: "#000",
+  marginBottom: 5,
+},
+
+totalPrice: {
+  fontSize: 18,
   fontWeight: "bold",
-  },
+},
 });
 
 export default ProductDetail;
